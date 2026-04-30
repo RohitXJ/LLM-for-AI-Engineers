3047487ee8db4510281c076e593046fca9d72cb1
Rohit Gomes <gomesrohit92@gmail.com>
2026-04-30 12:31:02 +0530

    Update main.py
    Semantic Chunking added

--- Module-01-Retrieval-Foundation/05-Naive-RAG-Loop/Project/main.py
+++ Module-01-Retrieval-Foundation/05-Naive-RAG-Loop/Project/main.py
@@ -2,8 +2,13 @@ import asyncio, argparse, os, datetime
 from pathlib import Path
 import chromadb
 from chromadb.utils import embedding_functions
+from sklearn.metrics.pairwise import cosine_similarity
+import numpy as np
+
 collection_name = "DOC_CHAT"
-VDB_loc = "Module-01-Retrieval-Foundation\05-Naive-RAG-Loop\Project\chroma_db"
+VDB_loc = f"./chroma_db"
+
+###-----CHROMA-DB-----###
 
 def chroma_init()->object:
     """
@@ -15,7 +20,11 @@ def chroma_init()->object:
         name=collection_name,
         embedding_function=default_ef
     )
+    global EMBED_MODEL
+    EMBED_MODEL = collection._embedding_function
     return collection
+
+###-----FILE and DATA ENTRY-----###
 
 def file_dump(dir_path, collection):
     dir_path = Path(dir_path)
@@ -28,14 +37,43 @@ def file_dump(dir_path, collection):
             print(f"Skipping {filename}: Already exists in DB.")
             return True
         else:
+            print(f"Dumping {filename} into DB.")
             return False
-    
+    #Put to Async later
+    def dump_to_vector_DB(documents,metadatas):
+        print(documents)
+
     #Put to Async later
     print(file_paths)
     for f in file_paths:
         if not file_injested_chk(os.path.basename(f)):
             documents = injest_data_pool(f)
-        print(documents)
+            chunks = semantic_chunking(TEXT=documents['content'])
+            dump_to_vector_DB(documents=chunks,metadatas=documents['metadata'])
+
+
+def semantic_chunking(TEXT,percentile_threshold=95,chunk_size=500,chunk_overlap=50):
+    from langchain_experimental.text_splitter import SemanticChunker
+    from langchain_text_splitters import RecursiveCharacterTextSplitter
+    # 1. Semantic Splitting (Meaning-based)
+    semantic_splitter = SemanticChunker(
+        embeddings=EMBED_MODEL, 
+        breakpoint_threshold_type="percentile",
+        breakpoint_threshold_amount=percentile_threshold,
+    )
+    # 2. Refined Splitting (Size & Overlap-based)
+    final_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=chunk_size,
+        chunk_overlap=chunk_overlap,
+        length_function=lambda x: len(x.split()), # Force word-count instead of characters
+        separators=["\n\n", "\n", " ", ""]
+    )
+
+    # Execution
+    docs = semantic_splitter.create_documents([TEXT])
+    final_chunks = final_splitter.split_documents(docs)
+    return final_chunks
+
 
 def injest_data_pool(file):
     path = Path(file)
@@ -69,6 +107,8 @@ def injest_data_pool(file):
 
     except Exception as e:
         raise Exception(f"Error processing {file}: {str(e)}")
+
+###-----MAIN CONTROLLER-----###
 
 def main(file_dir):
     collection = chroma_init()
